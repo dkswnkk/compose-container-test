d02497680c6ce1ab0cb6ce7eb592477432f0acc0
dkswnkk <jjoy9810@gmail.com>
2023-10-13 00:33:13 +0900

    feat: test setting

--- src/test/java/com/example/composecontainertest/ComposeContainerTestApplicationTests.java
+++ src/test/java/com/example/composecontainertest/ComposeContainerTestApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.composecontainertest;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ComposeContainerTestApplicationTests {
+class ComposeContainerTestApplicationTests extends IntegrationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
