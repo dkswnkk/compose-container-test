100da0cc9212e845aaa13a56b0201559e4dccc34
dkswnkk <jjoy9810@gmail.com>
2023-10-12 23:21:48 +0900

    Initial commit

---
+++ src/test/java/com/example/composecontainertest/ComposeContainerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.composecontainertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposeContainerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
