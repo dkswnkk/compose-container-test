d1db82f6ed5a0399c045c33850410d3b594f65c3
dkswnkk <jjoy9810@gmail.com>
2023-10-13 23:31:07 +0900

    feat: redis setting

--- src/test/java/com/example/composecontainertest/IntegrationTest.java
+++ src/test/java/com/example/composecontainertest/IntegrationTest.java
@@ -1,7 +1,8 @@
 package com.example.composecontainertest;
 
+import com.redis.testcontainers.RedisContainer;
 import org.jetbrains.annotations.NotNull;
-import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
@@ -10,70 +11,65 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.containers.wait.strategy.WaitStrategy;
 
 import java.io.File;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 통합 테스트를 위한 기본 클래스입니다.
- * Docker 환경을 설정하고 테스트를 위한 애플리케이션 컨텍스트를 구성합니다.
- */
-@Ignore
 @SpringBootTest
 @Transactional
 @ContextConfiguration(initializers = IntegrationTest.IntegrationTestInitializer.class)
 public class IntegrationTest {
 
-    // RDBMS를 위한 DockerComposeContainer의 싱글턴 인스턴스입니다.
-    private static final DockerComposeContainer<?> RDBMS = createRDBMSContainer();
+    private static DockerComposeContainer rdbms;
+    private static RedisContainer redis;
 
-    /**
-     * RDBMS를 위한 Docker Compose 컨테이너를 생성하고 시작합니다.
-     * @return DockerComposeContainer 인스턴스.
-     */
-    private static DockerComposeContainer<?> createRDBMSContainer() {
-        DockerComposeContainer<?> container = new DockerComposeContainer<>(new File("infra/test/docker-compose.yaml"))
-                .withExposedService("local-db", 3306, createWaitStrategy(".*ready for connections.*"))
-                .withExposedService("local-db-migrate", 0, createWaitStrategy("(.*Successfully applied.*)|(.*Successfully validated.*)"));
-        container.start();
-        return container;
+    @BeforeAll
+    public static void setupContainers() {
+        rdbms = new DockerComposeContainer(new File("infra/test/docker-compose.yaml"))
+                .withExposedService(
+                        "local-db",
+                        3306,
+                        Wait.forLogMessage(".*ready for connections.*", 1)
+                                .withStartupTimeout(Duration.ofSeconds(300))
+                )
+                .withExposedService(
+                        "local-db-migrate",
+                        0,
+                        Wait.forLogMessage("(.*Successfully applied.*)|(.*Successfully validated.*)", 1)
+                                .withStartupTimeout(Duration.ofSeconds(300))
+                );
+        redis = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("6"));
+
+        rdbms.start();
+        redis.start();
     }
 
-    /**
-     * Docker 서비스를 위한 대기 전략을 생성합니다.
-     * @param logMessagePattern 대기할 로그 메시지 패턴.
-     * @return 대기 전략.
-     */
-    private static WaitStrategy createWaitStrategy(String logMessagePattern) {
-        return Wait.forLogMessage(logMessagePattern, 1).withStartupTimeout(Duration.ofSeconds(300));
-    }
-
-
-    /**
-     * 스프링 애플리케이션 컨텍스트의 초기화기입니다.
-     * RDBMS 컨테이너 세부 정보를 기반으로 데이터 소스 속성을 설정합니다.
-     */
     static class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
         @Override
         public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
-            Map<String, String> properties = createDataSourceProperties();
+            Map<String, String> properties = new HashMap<>();
+
+            setDatabaseProperties(properties);
+            setRedisProperties(properties);
+
             TestPropertyValues.of(properties).applyTo(applicationContext);
         }
 
-        /**
-         * RDBMS 컨테이너 세부 정보를 기반으로 데이터 소스 속성을 생성합니다.
-         * @return 데이터 소스 속성 맵.
-         */
-        private Map<String, String> createDataSourceProperties() {
-            Map<String, String> properties = new HashMap<>();
-            String rdbmsHost = RDBMS.getServiceHost("local-db", 3306);
-            int rdbmsPort = RDBMS.getServicePort("local-db", 3306);
+        private void setDatabaseProperties(Map<String, String> properties) {
+            String rdbmsHost = rdbms.getServiceHost("local-db", 3306);
+            int rdbmsPort = rdbms.getServicePort("local-db", 3306);
+
             properties.put("spring.datasource.url", "jdbc:mysql://" + rdbmsHost + ":" + rdbmsPort + "/container");
-            return properties;
+        }
+
+        private void setRedisProperties(Map<String, String> properties) {
+            String redisHost = redis.getHost();
+            Integer redisPort = redis.getFirstMappedPort();
+
+            properties.put("spring.redis.host", redisHost);
+            properties.put("spring.redis.port", redisPort.toString());
         }
     }
 }
